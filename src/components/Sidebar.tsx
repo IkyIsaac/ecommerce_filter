450a127f56dd950acb16ab6372094f55313ef0e9
IkyIsaac <isaackitomari33@gmail.com>
2024-09-16 20:34:32 +0300

    do initialized the project Complete the filter logic and ui

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,146 @@
+import { useEffect, useState } from "react";
+interface Product {
+  category: "category";
+}
+
+interface FetchResponse {
+  products: Product[];
+}
+
+const Sidebar = () => {
+  const {
+    searchQuery,
+    setSearchQuery,
+    selectedCategory,
+    setSelectedCategory,
+    minPrice,
+    setMinPrice,
+    maxPrice,
+    setMaxPrice,
+    keyword,
+    setKeyword,
+  } = useFilter();
+
+  const [categories, setCategories] = useState<string[]>([]);
+  console.log(categories);
+
+  const [keywords] = useState<string[]>([
+    "apple",
+    "watch",
+    "Fashion",
+    "trend",
+    "shoes",
+    "shirt",
+  ]);
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await fetch("https://dummyjson.com/products");
+        const data: FetchResponse = await response.json();
+        const uniqueCategories = Array.from(
+          new Set(data.products.map((product) => product.category))
+        );
+        setCategories(uniqueCategories);
+      } catch (error) {
+        console.log("Failed to fetch data: ", error);
+      }
+    };
+
+    fetchCategories();
+  }, []);
+
+  const handleMinPriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setMinPrice(value ? parseFloat(value) : undefined);
+  };
+
+  const handleMaxPriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setMaxPrice(value ? parseFloat(value) : undefined);
+  };
+  const handleRadioChangeCategories = (category: string) => {
+    setSelectedCategory(category);
+  };
+
+  const  handleKeywordClick=(keyword: string)=>{
+    setKeyword(keyword)
+  }
+
+  const handleResetFilters=()=>{
+    setSearchQuery('')
+    selectedCategory('')
+    setMinPrice('')
+    setMaxPrice('')
+    setKeyword('')
+  }
+
+  return (
+    <div className="w-64 h-screen p-5">
+      <h1 className="text-2xl font-bold mb-10 mt-4">React Store</h1>
+      <section>
+        <input
+          type="text"
+          className="border-2 rounded px-2 sm:mb-0"
+          placeholder="Search Product"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <div className="flex justify-center items-center">
+          <input
+            type="text"
+            className="border-2 px-5 py-3 mb-3 w-full"
+            placeholder="Min"
+            value={minPrice ?? ""}
+            onChange={handleMinPriceChange}
+          />
+          <input
+            type="text"
+            className="border-2 px-5 py-3 mb-3 w-full"
+            placeholder="max"
+            value={maxPrice ?? ""}
+            onChange={handleMaxPriceChange}
+          />
+        </div>
+
+        {/* Categories */}
+        <div className="mb-5">
+          <h2 className="text-xl font-semibold mb-3">Categories</h2>
+        </div>
+        {categories.map((category, index) => (
+          <label key={index} className="block mb-2">
+            <input
+              type="radio"
+              name="category"
+              value={category}
+              onChange={() => handleRadioChangeCategories(category)}
+              className="mr-2 w-[16px] h-[16px]"
+              checked={selectedCategory == category}
+            />
+            {category.toUpperCase()}
+          </label>
+        ))}
+      </section>
+      <section>
+        <div className="mb-5">
+          <h2 className="font-semibold text-xl mb-3">Keywords</h2>
+        </div>
+        {keywords.map((keyword, index) => (
+          <button
+            key={index}
+            onClick={()=> handleKeywordClick(keyword)}
+            className="block mb-2 py-2 w-full text-left border rounded hover:bg-gray-200"
+          >
+            {keyword.toUpperCase()}
+          </button>
+        ))}
+
+        <button onClick={handleResetFilters} className="w-full mb-[4rem] py-2 bg-black text-white rounded mt-5">
+          Reset Filters
+        </button>
+      </section>
+    </div>
+  );
+};
+
+export default Sidebar;
